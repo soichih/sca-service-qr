d97f27c4b106d1455afabf9982988962038609f1
Soichi Hayashi <hayashis@iu.edu>
2016-04-07 23:38:42 +0000

    removed -it

--- start.sh
+++ start.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/bash
 
-docker run -it --rm \
+docker run --rm \
         -v `pwd`/../input:/input \
         -v `pwd`/output:/output \
-        soichih/odi-quickreduce ./podi_collectcells.py -nonlinearity /input/o20160217T192119.2.11.fits.fz /output/%OBJECT.fits
+        soichih/odi-quickreduce ./podi_collectcells.py -nonlinearity /input/o20160217T192119.2.11.fits.fz /output/%OBJECT.fits \
+    &
 
 echo "{\"hello\":\"there\"}" > products.json
