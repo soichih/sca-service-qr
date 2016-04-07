1831167e0fa22b72177d42da610c9a73c92ab8b5
Soichi Hayashi <hayashis@iu.edu>
2016-04-07 20:34:54 +0000

    added docker run

--- start.sh
+++ start.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/bash
 
-echo "{\"hello\":\"there\"}" > hello.json
+docker run -it --rm \
+        -v `pwd`/../input:/input \
+        -v `pwd`/output:/output \
+        soichih/odi-quickreduce ./podi_collectcells.py -nonlinearity /input/o20160217T192119.2.11.fits.fz /output/%OBJECT.fits
+
+echo "{\"hello\":\"there\"}" > products.json
