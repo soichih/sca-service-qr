d684c0e5cd6ae8697ae603ca094c5d2f75397503
root <root@q6.sca.iu.edu>
2016-04-08 14:50:07 -0400

    added SCA_PROGRESS_URL -e for docker run

--- start.sh
+++ start.sh
@@ -2,13 +2,20 @@
 
 
 #create list.txt using all dirctories existing in the input directory
-ls ../input > ../input/list.txt
+ls  ../input | awk '{print "/input/"$1}' > ../input/list.txt
 
-dockerid=$(docker run --rm \
+echo $SCA_PROGRESS_URL
+docker run \
+	-e SCA_PROGRESS_URL=$SCA_PROGRESS_URL \
         -v `pwd`/../input:/input \
         -v `pwd`/output:/output \
-        iusca/dockqr ./podi_multicollect.py -fromfile=/input/list.txt -formatout=/output/%OBSID.fits -nonlinearity \
-    &)
+	-d iusca/dockqr ./podi_multicollect.py -fromfile=/input/list.txt -formatout=/output/%OBSID.fits -nonlinearity \
+    &
+
+#this should get the docker container id
+dockerid=`docker ps -l -q`
+
+#-e SCA_PROGRESS_URL="https://soichi7.ppa.iu.edu/api/progress/status/_sca.5707063c4f94be7d71ce1297.570706494f94be7d71ce1298" \
 
 echo "started docker container $dockerid"
 echo $dockerid > docker.id
