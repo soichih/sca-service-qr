5677aaf5357fc43aa41f14355f155be2337cb9e1
Soichi Hayashi <hayashis@iu.edu>
2016-04-12 00:35:40 +0000

    added code to remove docker container

--- status.sh
+++ status.sh
@@ -11,9 +11,11 @@ case "$status" in
         exitcode=`docker inspect $dockerid | jq -r '.[0].State.ExitCode'`
         if [ $exitcode -eq 0 ]; then
             echo "$dockerid finished successfully"
+            docker rm $dockerid
             exit 1
         else
             echo "$dockerid exited with code:$exitcode"
+            docker rm $dockerid
             exit 2
         fi
         ;;
