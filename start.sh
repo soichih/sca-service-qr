40709e850873157a67c17e566e9c3c267314eb2d
Soichi Hayashi <soichih@gmail.com>
2016-06-20 20:10:35 +0000

    installed jq

--- start.sh
+++ start.sh
@@ -1,5 +1,17 @@
 #!/usr/bin/bash
 
+#make sure jq is installed on $SCA_SERVICE_DIR
+if [ ! -f $SCA_SERVICE_DIR/jq ];
+then
+        echo "installing jq"
+        wget https://github.com/stedolan/jq/releases/download/jq-1.5/jq-linux64 -O $SCA_SERVICE_DIR/jq
+        chmod +x $SCA_SERVICE_DIR/jq
+fi
+
+input_task_id=`$SCA_SERVICE_DIR/jq -r '.input_task_dir' config.json`
+echo "dumping products.json"
+cat ../$input_task_id/products.json
+
 #create list.txt using all dirctories existing in the input directory
 ls  ../input | awk '{print "/input/"$1}' > ../input/list.txt
 
