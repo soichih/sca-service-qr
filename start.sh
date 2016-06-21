084493db20500a4fdf4d5d9d9c8b923ea03cda64
Soichi Hayashi <soichih@gmail.com>
2016-06-21 01:49:31 +0000

    prefixed dark

--- start.sh
+++ start.sh
@@ -41,7 +41,7 @@ fi
 
 dark=`$SCA_SERVICE_DIR/jq -r '.[0] .dark' ../$input_task_id/products.json`
 if [ ! $dark == "null" ]; then
-    opts="$opts --dark=$dark"
+    opts="$opts --dark=/input/$dark"
 fi
 
 echo "using opts"
